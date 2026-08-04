ab37f1a01dd796230501f1252a4f8df170cd4d16
Ndungu-Jn <ndungulink@gmail.com>
2026-08-04 13:13:48 +0300

    add initial hello.py with basic print statements and comments

---
+++ hello.py
@@ -0,0 +1,8 @@
+print("Hello world, this is the first program for today. testing")
+
+# printing  this line
+print("This is my second line today")
+
+# comments
+
+# this is another comment. a single line comment.
