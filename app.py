3f29dd6ac4983384254e7d25856ee3c80a60b203
Ndungu-Jn <ndungulink@gmail.com>
2026-08-07 11:34:51 +0300

    add app.py with Python recap examples and print statements

---
+++ app.py
@@ -0,0 +1,42 @@
+print("-----------------")
+print("   Recap Python  ")
+print("-----------------")
+
+# \n that is for new line
+# \t tab
+# \b backspace
+# \ skips. since it a special.
+# \\ backslash itself
+
+print("Hello \"Python\"")
+
+print("Message1\n\n\nmessage2")
+
+print("Your learning path:\n\t- Python Basics\n\t- Data Engineering\n\t- AI")
+
+
+x = 1
+print(x)
+x = 2
+print(x)
+
+
+y = x+3
+print(y)
+
+
+name = "John"
+print(f"My name is {name}")
+print("My name is", name)
+print("I am having a recap of python\n\n")
+
+
+# Python executes line by line
+print("info@datawithbaraa.com")
+print("support@datawithbaraa.com")
+print("www.datawithbaraa.com\n")
+
+email = "datawithbaraa.com"
+print(f"info@{email}")
+print(f"support@{email}")
+print(f"www.{email}")
