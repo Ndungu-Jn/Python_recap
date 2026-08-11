ae776195b03483a8bc62729f101b9323331c7578
Ndungu-Jn <ndungulink@gmail.com>
2026-08-11 15:40:44 +0300

    Add numbers.py with examples of data types, mathematical operations, and random number generation

---
+++ numbers.py
@@ -0,0 +1,28 @@
+import math
+import random
+
+x = 5
+y = 5.7
+z = 2 + 3j
+
+print(type(y))
+
+x = "24"
+print(type(x))
+x = float(x)
+print(x)
+print(type(x))
+
+print(2 + 3)
+print(abs(2 - 10))
+
+price = 35.546764736
+print(round(price))
+print(round(price, 2))
+
+print(math.floor(price))
+print(math.ceil(price))
+
+print(math.trunc(price))
+
+print(random.randint(1, 6))
