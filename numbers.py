c169db30181f2528fab1cd3da8887280f8cf4483
Ndungu-Jn <ndungulink@gmail.com>
2026-08-12 15:50:13 +0300

    Enhance numbers.py with additional examples of random number generation, logic, and control flow

--- numbers.py
+++ numbers.py
@@ -25,4 +25,45 @@ print(math.ceil(price))
 
 print(math.trunc(price))
 
-print(random.randint(1, 6))
+random_number = random.randint(1, 100)
+print(random_number)
+if random_number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+# Logic and operators
+# control flow
+# Conditional statements
+
+print(True)
+print(False)
+
+email = ""
+phone = "4646637346"
+username = ""
+
+# Allows registration
+# if any field is filled
+print(any([email, phone, username]))
+
+# Allows registration
+# if any field is filled
+print(all([email, phone, username]))
+
+# check if the system is under pressure
+cpu_usage = 70
+memory_usage = 95
+print(cpu_usage > 90 or memory_usage > 90)
+
+# Cheking user credentials before log in
+email = True
+password = False
+print(email and password)
+
+
+is_logged_in = True
+is_guest = False
+is_banned = True
+
+print((is_logged_in or is_guest) and not is_banned)
